d8962ee986fac0568e97e1158cfff6b6549d86bb
gouf <innocent.zero@gmail.com>
2018-07-26 13:28:17 +0900

    Add model annotation for annotate gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  memo       :text
+#  author     :string
+#  picture    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
 end
